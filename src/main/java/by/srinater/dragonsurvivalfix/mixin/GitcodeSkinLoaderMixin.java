62a0680f2432bfbe5f8953dd9e192b0af445ee7b
FirDragon <2511767981@qq.com>
2024-07-07 15:58:11 +0800

    Initialize

--- src/main/java/by/srinater/dragonsurvivalfix/mixin/GitcodeSkinLoaderMixin.java
+++ src/main/java/by/srinater/dragonsurvivalfix/mixin/GitcodeSkinLoaderMixin.java
@@ -10,8 +10,6 @@ import com.google.gson.Gson;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 
 import java.io.BufferedReader;
 import java.io.IOException;
